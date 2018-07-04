90a01111830b446468a1a03d949696cef8328862
Fred K <frederico-klein@plymouth.ac.uk>
2018-07-04 13:28:37 +0100

    added nifty messages to tell you who you are connected to...

--- scripts/dockerlauncher.sh
+++ scripts/dockerlauncher.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 # Docker Launcher script. Launches helloworld from Docker.
-
-docker run hello-world
+echo "Running a remote docker image on ssh port: $1. I currently cannot tell which computer is this!"
+#ssh localhost -p $1 "docker run hello-world"
+ssh localhost -p $1 "ls -la $2/scripts/"
---
+++ scripts/dockerrun.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo "Running on host: `hostname`"
+echo ""
+echo "IP: `hostname -I`"
